d3901b99fd18649122b6aee36e4b3b64d0223649
unknown <huzefaratlam63@gmail.com>
2025-05-07 15:32:09 +0530

    nothing

--- utils/helper.js
+++ utils/helper.js
@@ -12,7 +12,7 @@ export const createJwtAndSendCookie = (res,cookieName,rememberMe,statusCode,resO
 
   res.cookie(cookieName,token,{
     expires:new Date(Date.now() + rememberMe * 24 * 60 * 60 * 1000),
-    secure:false,
-    httpOnly:false,
+    secure:true,
+    httpOnly:true,
   }).status(statusCode).json(process.env.ENVIROMENT ==='production' ? resObject:{token});
 }
