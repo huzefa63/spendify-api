1a9eb8f70ed663e9055e1abe2ae03ef9d9f92a86
unknown <huzefaratlam63@gmail.com>
2025-05-13 18:49:16 +0530

    back to old cors

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ import { faker } from "@faker-js/faker";
 const app = express();
 
 // required middlewares
-app.use(
-  cors({ origin: '*'})
-);
 // app.use(
-//   cors({ origin: process.env.URL, credentials: true })
+//   cors({ origin: '*'})
 // );
+app.use(
+  cors({ origin: process.env.URL, credentials: true })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
