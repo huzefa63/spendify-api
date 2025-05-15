1eb6e8dd1a73f8c81e13252c815bcc41bc101c90
unknown <huzefaratlam63@gmail.com>
2025-05-15 19:08:05 +0530

    fixed pre middleware

--- models/transaction.js
+++ models/transaction.js
@@ -43,13 +43,24 @@ schema.pre('save',function(next){
   next();
 })
 
-schema.pre('findOneAndUpdate',function(next){
-  const month = ['jan','feb','march','april','may','june','july','aug','sep','oct','nov','dec'];
-  this.year = this.date.getFullYear()
-  this.month = month[this.date.getMonth()];
-  this.monthNumber = month.indexOf(this.month);
+schema.pre('findOneAndUpdate', function (next) {
+  const update = this.getUpdate();
+
+  if (update.date) {
+    const date = new Date(update.date); // ensure it's a Date object
+    const months = ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'];
+    
+    update.year = date.getFullYear().toString();
+    update.month = months[date.getMonth()];
+    update.monthNumber = date.getMonth() + 1; // 1-based (Jan = 1)
+
+    // Reassign the updated fields
+    this.setUpdate(update);
+  }
+
   next();
-})
+});
+
 
 const model = mongoose.model('Transaction',schema);
 export default model;
